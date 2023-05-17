5df12cce708dbc6183a4648b2b1af5a5f801db7b
Fnord <19fnord@gmail.com>
2023-05-17 22:36:28 +0200

    enable GNOME keyring

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,6 +18,9 @@
   networking.hostName = "pharasma"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  # Enable GNOME keyring
+  security.pam.services.lightdm.enableGnomeKeyring = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
